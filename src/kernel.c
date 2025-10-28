6c03c32979496c5c2c969400e38d62cb9850d4b8
einarthesad <elentarion@o2.pl>
2025-10-28 15:15:54 +0100

    Play with VGA colors

--- src/kernel.c
+++ src/kernel.c
@@ -3,10 +3,12 @@
 extern void idt_init(void);
 extern void vga_clear(void);
 extern void vga_use_as_console(void);
+extern void vga_set_color(uint8_t attr);
 
 void kmain(void) {
     idt_init();
     vga_use_as_console();
+    vga_set_color(0b00011011);
     vga_clear();
 
     printf("OSlet has booted. VGA width=%d height=%d\n", 80, 25);
