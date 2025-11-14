a7d6af2c090bfe75c1bd6134cada9f9b431d4ab0
einarthesad <elentarion@o2.pl>
2025-11-14 13:45:49 +0100

    Rewrite to use new library

--- src/bin/fetchlet.c
+++ src/bin/fetchlet.c
@@ -1,44 +1,58 @@
-/* neofetch-like system information display
- * good for testing early binary loading */
+/* neofetch-like system information display */
 
 #include "../syscall.h"
+#include "../lib/stdio.h"
+
+#define YEAR 2025
+
+static int mem = 32;
 
 void _start(void) {
-    sys_setcolor(0,9);
-    sys_write("\n  default user\n");
-    sys_setcolor(0,7);
-    sys_write("  ------------\n");
-    sys_setcolor(0,9);
-    sys_write("  OS");
-    sys_setcolor(0,7);
-    sys_write(": Codename osLET\n");
-    sys_setcolor(0,9);
-    sys_write("  Kernel");
-    sys_setcolor(0,7);
-    sys_write(": Development Kernel 0.3\n");                                                
-    sys_setcolor(0,9);
-    sys_write("  Shell");
-    sys_setcolor(0,7);
-    sys_write(": oslet-v03\n");
-    sys_setcolor(0,9);
-    sys_write("  CPU");
-    sys_setcolor(0,7);
-    sys_write(": i386 Compatible\n"); /* placeholder */
-    sys_setcolor(0,9);
-    sys_write("  Memory");
-    sys_setcolor(0,7);
-    sys_write(": 32 MiB\n"); /* placeholder */
-    sys_setcolor(0,9);
-    sys_write("  Author");
-    sys_setcolor(0,7);
-    sys_write(": EinarTheSad\n");
-    sys_setcolor(0,9);
-    sys_write("  Year");
-    sys_setcolor(0,7);
-    sys_write(": 2025\n");
-    sys_setcolor(0,9);
-    sys_write("  Repository");
-    sys_setcolor(0,7);
-    sys_write(": https://github.com/einarthesad/oslet\n\n");
+    sys_setcolor(0, 9);
+    printf("\n  default user\n");
+    
+    sys_setcolor(0, 7);
+    printf("  ------------\n");
+    
+    sys_setcolor(0, 9);
+    printf("  OS");
+    sys_setcolor(0, 7);
+    printf(": Codename osLET\n");
+    
+    sys_setcolor(0, 9);
+    printf("  Kernel");
+    sys_setcolor(0, 7);
+    printf(": Development Kernel 0.3\n");
+    
+    sys_setcolor(0, 9);
+    printf("  Shell");
+    sys_setcolor(0, 7);
+    printf(": oslet-v03\n");
+    
+    sys_setcolor(0, 9);
+    printf("  CPU");
+    sys_setcolor(0, 7);
+    printf(": i386 Compatible\n");
+    
+    sys_setcolor(0, 9);
+    printf("  Memory");
+    sys_setcolor(0, 7);
+    printf(": %d MiB\n", mem);
+    
+    sys_setcolor(0, 9);
+    printf("  Author");
+    sys_setcolor(0, 7);
+    printf(": EinarTheSad\n");
+    
+    sys_setcolor(0, 9);
+    printf("  Year");
+    sys_setcolor(0, 7);
+    printf(": %d\n", YEAR);
+    
+    sys_setcolor(0, 9);
+    printf("  Repository");
+    sys_setcolor(0, 7);
+    printf(": https://github.com/einarthesad/oslet\n\n");
+    
     sys_exit();
 }
