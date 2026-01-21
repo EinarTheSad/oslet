bae9d93d47eecfb08f6eef87c388d4cd7fdd12e8
einarthesad <elentarion@o2.pl>
2026-01-21 13:55:48 +0100

    Fix icon slot system

--- src/win/wm.h
+++ src/win/wm.h
@@ -2,6 +2,13 @@
 #include "wm_config.h"
 #include "../syscall.h"
 
+// Free icon slot tracking
+typedef struct {
+    int x, y;
+} icon_slot_t;
+
+#define WM_MAX_FREE_SLOTS 32
+
 // Window manager state
 typedef struct {
     gui_form_t *windows[WM_MAX_WINDOWS];  // Array of window pointers
@@ -12,6 +19,8 @@ typedef struct {
     uint32_t last_icon_click_time;         // For double-click detection
     int last_icon_click_x;
     int last_icon_click_y;
+    icon_slot_t free_slots[WM_MAX_FREE_SLOTS];  // Pool of free icon slots
+    int free_slot_count;                         // Number of available free slots
 } window_manager_t;
 
 void wm_init(window_manager_t *wm);
@@ -25,6 +34,7 @@ gui_form_t* wm_get_window_at(window_manager_t *wm, int x, int y);
 
 void wm_draw_all(window_manager_t *wm);
 void wm_get_next_icon_pos(window_manager_t *wm, int *out_x, int *out_y);
+void wm_release_icon_slot(window_manager_t *wm, int x, int y);
 
 void wm_set_icon_click(window_manager_t *wm, uint32_t time, int x, int y);
 int wm_is_icon_doubleclick(window_manager_t *wm, uint32_t time, int x, int y);
