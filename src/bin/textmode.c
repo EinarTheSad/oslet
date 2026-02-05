f75f6244983ce8731f42dfea22bf13de74751de4
einarthesad <elentarion@o2.pl>
2026-02-05 21:22:42 +0100

    End running programs when exiting to text mode

--- src/bin/textmode.c
+++ src/bin/textmode.c
@@ -1,4 +1,5 @@
 #include "progmod.h"
+#include "progman.h"
 #include "../syscall.h"
 #include "../lib/string.h"
 
@@ -19,6 +20,20 @@ static int textmode_init(prog_instance_t *inst) {
 
     if (!is_shell_running()) {
         sys_spawn_async("SHELL.ELF");
+    }
+
+    /* End all running graphical programs for now */
+    uint16_t to_close[PROGMAN_INSTANCES_MAX];
+    int tc = 0;
+    for (int i = 0; i < PROGMAN_INSTANCES_MAX; i++) {
+        prog_instance_t *pi = progman_get_instance(i);
+        if (pi && pi->state == PROG_STATE_RUNNING) {
+            to_close[tc++] = pi->instance_id;
+        }
+    }
+
+    for (int i = 0; i < tc; i++) {
+        progman_close(to_close[i]);
     }
 
     sys_gfx_exit();
